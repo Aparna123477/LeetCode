37f2216520c66237f38d44aa1921792311280ec9
Aparna123477 <jmdaparna@gmail.com>
2026-02-04 11:10:58 +0530

    Time: 6 ms (86.57%), Space: 44.2 MB (50.64%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,28 +1,25 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int k=s1.length();
-
-        for(int i=0;i<=s2.length()-k;i++){
-           String sub= s2.substring(i,i+k);
-           if(isAnagram(s1,sub))return true;
-        }
-        return false;
-   }
-    public  boolean isAnagram(String s, String t) {
         int[] f1=new int[26];
-        for(char ch:s.toCharArray()){
+        int k=s1.length();
+        for(char ch:s1.toCharArray()){
             f1[ch-'a']++;
-
         }
         int[] f2=new int[26];
-        for(char ch:t.toCharArray()){
+        for(int i=0;i<s2.length();i++){
+            char ch=s2.charAt(i);
             f2[ch-'a']++;
-        }
+            if(i<k-1){
+                continue;
+            }
+            if(Arrays.equals(f1,f2)) return true;
+            //subtract first element
+            int sin=i-(k-1);
+            char r=s2.charAt(sin);
+            f2[r-'a']--;
 
-        return Arrays.equals(f1,f2);
         }
-        
+        return false;
+
     }
-    
-
-
+}
