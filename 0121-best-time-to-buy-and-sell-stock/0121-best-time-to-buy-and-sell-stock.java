82b7f43fc6db9ecdfc3380afd2538bb043ac7181
Aparna123477 <jmdaparna@gmail.com>
2026-03-04 22:35:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max=0;
+        int l=prices.length;
+        int buy=Integer.MAX_VALUE;
+
+        for(int i=0;i<l;i++){
+            buy=Math.max(buy,prices[i]);
+            int sell=prices[i];
+            int profit=sell-buy;
+            max=Math.max(max,profit);
+
+        }
+        return max;
+    }
+}
