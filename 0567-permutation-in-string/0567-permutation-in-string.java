d27642ebe02b7d9384ca61761fce4316f1cfdc33
Aparna123477 <jmdaparna@gmail.com>
2026-03-12 14:44:52 +0530

    Time: 166 ms (11.35%), Space: 46.7 MB (25.13%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean checkInclusion(String s1, String s2) {
         char[] arr1=s1.toCharArray();
         int k=s1.length();
-        for(int i=0;i<s2.length()-k;i++){
+        for(int i=0;i<=s2.length()-k;i++){
             String sub=s2.substring(i,i+k);
             if(isAnagram(s1,sub)) return true;
         }
