e31f421750353c0df0119366bac03f385904657e
Aparna123477 <jmdaparna@gmail.com>
2026-03-12 14:40:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,24 +1,23 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+        char[] arr1=s1.toCharArray();
         int k=s1.length();
-        for(int i=0;i<=s2.length()-k;i++){
+        for(int i=0;i<s2.length()-k;i++){
             String sub=s2.substring(i,i+k);
             if(isAnagram(s1,sub)) return true;
         }
-        return false;}
-
-     public static boolean isAnagram(String s,String n){
-            int[] f1=new int[26];
-            for(char ch: s.toCharArray()){
-                f1[ch-'a']++;
-            }
-            int[] f2=new int[26];
-            for(char ch:n.toCharArray()){
-                f2[ch-'a']++;
-            }
-            return Arrays.equals(f1,f2);
-        }
-      
-
-        
+        return false;
     }
+    public static boolean isAnagram(String s1,String s2){
+        int[] fr1=new int[26];
+        for(char ch:s1.toCharArray()){
+            fr1[ch-'a']++;
+        }
+        int[] fr2=new int[26];
+        for(char ch:s2.toCharArray()){
+            fr2[ch-'a']++;
+        }
+        return Arrays.equals(fr1,fr2);
+       
+    }
+}
