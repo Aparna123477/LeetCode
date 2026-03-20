e2cdd26c45e85eea3a451fe41d48429e7f9455d5
Aparna123477 <jmdaparna@gmail.com>
2026-03-20 09:42:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1537-get-the-maximum-score/1537-get-the-maximum-score.java
+++ 1537-get-the-maximum-score/1537-get-the-maximum-score.java
@@ -1,33 +1,27 @@
 class Solution {
     public int maxSum(int[] nums1, int[] nums2) {
         int i=0,j=0;
-        long s1=0,s2=0;
-        long sum=0;
-        while(i<nums1.length && j<nums2.length){
-            if(nums1[i]<nums2[j]){
-                s1+=nums1[i];
-                i++;
-            }
-            else if(nums2[j]<nums1[i]){
-                s2+=nums2[j];
-                j++;
-            }       
-            else{
-                sum+=Math.max(s1,s2)+nums1[i];
-                i++;
-                j++;
-                s1=0;
-                s2=0;
-            }
-        }
-        while(i<nums1.length){
-            s1+=nums1[i++];
+        long sum1=0,sum2=0,sum=0;
+        int n1=nums1.length,n2=nums2.length;
 
+        while(i<n1 && j<n2){
+            if(nums1[i]<nums2[j]) {
+                sum1+=nums1[i++];
+            }
+            else if(nums1[i]>nums2[j]){
+                sum2+=nums2[j++];
+            }
+            else{
+                sum+=Math.max(sum1,sum2)+nums1[i];
+                i++;
+                j++;
+                sum1=sum2=0;
+            }
         }
-        while(j<nums2.length){
-            s2+=nums2[j++];
-        }
-        sum+=Math.max(s1,s2);
-        return (int) (sum%(1e9+7));
+        while(i<n1) {sum1+=nums1[i++];}
+        while(j<n2){sum2+=nums2[j++];}
+        sum+=Math.max(sum1,sum2);
+        return (int)(sum%(1e9+7));
+
     }
 }
