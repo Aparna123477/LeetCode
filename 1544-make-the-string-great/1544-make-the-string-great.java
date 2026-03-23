76258551a98b0b4283d581b91d5fed9557e7e9cf
Aparna123477 <jmdaparna@gmail.com>
2026-03-23 09:53:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -1,32 +1,13 @@
 class Solution {
     public String makeGood(String s) {
-        Stack<Character> stack=new Stack<>();
-        for(char ch:s.toCharArray()){
-        if(!stack.isEmpty() && (Character.toLowerCase(ch)==Character.toLowerCase(stack.peek()) &&
-             Character.isUpperCase(ch) &&
-              Character.isLowerCase(stack.peek())))
-            {stack.pop();}
-
-
-            else if(!stack.isEmpty() && ( Character.isLowerCase(ch) &&
-             Character.isUpperCase(stack.peek()) &&
-              Character.toLowerCase(ch)==Character.toLowerCase(stack.peek()) ))
-             {stack.pop();
-             }
-
-
-            else{
-                stack.push(ch);
-            }
-
-
+        Stack<Character>stack=new Stack<>();
+        for(char c:s.toCharArray()){
+            if(!stack.isEmpty() && Math.abs(ch-stack.peek())==32)stack.pop();
         }
-        StringBuilder str=new StringBuilder("");
+        StringBuilder sb=new StringBuilder();
         while(!stack.isEmpty()){
-            str.append(stack.pop());
+            sb.append(stack.pop());
         }
-        return str.reverse().toString();
-
-        
+        retrun sb.reverse().toString();
     }
 }
