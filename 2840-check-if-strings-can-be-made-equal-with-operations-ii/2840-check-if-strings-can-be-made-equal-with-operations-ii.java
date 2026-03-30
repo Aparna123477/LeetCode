e296653eeeee754b53565b61ee9fc9c6a51bd296
Aparna123477 <jmdaparna@gmail.com>
2026-03-30 23:16:06 +0530

    Time: 7 ms (25%), Space: 48 MB (65.79%) - LeetHub

---
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean checkStrings(String s1, String s2) {
+       
+        int[] evenS1 = new int[26];
+        int[] oddS1 = new int[26];
+        int[] evenS2 = new int[26];
+        int[] oddS2 = new int[26];
+
+        for (int i = 0; i < s1.length(); i++) {
+            if (i % 2 == 0) {
+                evenS1[s1.charAt(i) - 'a']++;
+                evenS2[s2.charAt(i) - 'a']++;
+            } else {
+                oddS1[s1.charAt(i) - 'a']++;
+                oddS2[s2.charAt(i) - 'a']++;
+            }
+        }
+
+       
+        return Arrays.equals(evenS1, evenS2) && Arrays.equals(oddS1, oddS2);
+    }
+}
