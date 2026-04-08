86255063b594a469d85001469ef22cfba83d2e5a
APARNA SHARMA <jmdaparna@gmail.com>
2026-04-08 11:57:05 +0530

    Time: 3 ms (86.88%), Space: 47.3 MB (94.02%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int[] arr=new int[ratings.length];
+        Arrays.fill(arr,1);
+
+        for(int i=1;i<ratings.length;i++){
+           if(ratings[i]>ratings[i-1]) {arr[i]=arr[i-1]+1;}
+        }
+
+        for(int i=ratings.length-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1] && arr[i]<=arr[i+1]) {arr[i]=arr[i+1]+1;}
+        }
+        int max=0;
+        for(int i=0;i<arr.length;i++){
+            max+=arr[i];
+        }
+        return max;
+    }
+}
