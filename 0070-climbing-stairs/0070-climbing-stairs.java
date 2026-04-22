fbcc90464302bb6fb41280a0a9c440db16235a22
APARNA SHARMA <jmdaparna@gmail.com>
2026-04-22 11:32:16 +0530

    Time: 0 ms (100%), Space: 42.1 MB (38.44%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        int dp[]=new int[n+1];
+        return fun(0,n,dp);
+    }
+    int fun(int i,int n,int[] dp){
+        if(i==n) return 1;
+        if(i>n) return 0;
+        if(dp[i]!=0) return dp[i];
+        int ways1=fun(i+1,n,dp);
+        int ways2=fun(i+2,n,dp);
+        return dp[i]=ways1+ways2;
+    }
+}
