89a50020cad3f91ac5ca20db4c0f03bdbcd99e35
APARNA SHARMA <jmdaparna@gmail.com>
2026-04-25 16:53:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -3,18 +3,17 @@ class Solution {
         Stack<Integer>stack=new Stack<>();
         stack.push(-1);
         int ans=0;
-        
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-            if(ch=='(' )stack.push(i);
+            if(ch=='(')stack.push(i);
             else{
                 stack.pop();
-                if(stack.isEmpty()) {
+                if(stack.isEmpty()){
                     stack.push(i);
                 }
                 else{
                     int size=i-stack.peek();
-                    ans=Math.max(ans,size);
+                    ans=Math.max(size,ans);
                 }
             }
         }
