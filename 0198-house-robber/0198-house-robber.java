a6a1e8eeebdb21b02bacd6f0841d8665201ace92
APARNA SHARMA <jmdaparna@gmail.com>
2026-04-29 11:55:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,11 +1,12 @@
 class Solution {
     public int rob(int[] nums) {
-        int[] dp=new int[nums.length];
-        dp[0]=nums[0];
-        if(nums.length>1) dp[1]=Math.max(nums[0],nums[1]);
-        for(int i=2;i<nums.length;i++){
-            dp[i]=Math.max(dp[i-1],nums[i]+dp[i-2]);
+        int prev=0;
+        int prev2=0;
+        for(int i=0;i<nums.length;i++){
+            int curr=Math.max(prev,nums[i]+prev2);
+            prev2=prev;
+            prev=curr;
         }
-        return dp[nums.length-1];
+        return prev;
     }
 }
