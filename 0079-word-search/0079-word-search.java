de6e960b647bdbc2f6938afe6ec58c62c895bb07
APARNA SHARMA <jmdaparna@gmail.com>
2026-04-29 11:14:14 +0530

    Time: 139 ms (50.21%), Space: 43.1 MB (45.23%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -3,23 +3,23 @@ class Solution {
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[0].length;j++){
                 if(board[i][j]==word.charAt(0)){
-                    if(func(board,word,0,i,j)) return true;
+                    if(func(board,word,i,j,0)) return true;
                 }
             }
         }
         return false;
      
     }
-    boolean  func(char[][] board,String word,int idx,int r,int c){
+    boolean  func(char[][] board,String word,int r,int c,int idx){
         if(word.length()==idx)return true;
        if(r<0 || r>=board.length || c<0 || c>=board[0].length)return false;
        
-       if(board[r][c]!=word.charAt(0)) return false;
+       if(board[r][c]!=word.charAt(idx)) return false;
        char ch=board[r][c];
        board[r][c]=' ';
        boolean flag=false;
        flag|= func(board,word,r-1,c,idx+1);
-        flag|=func(board,word,,r+1,c,idx+1);
+        flag|=func(board,word,r+1,c,idx+1);
         flag|=func(board,word,r,c-1,idx+1);
         flag|=func(board,word,r,c+1,idx+1);
         board[r][c]=ch;
