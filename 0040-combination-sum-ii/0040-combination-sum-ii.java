5846c25f3f8352db624f85694ab16fa1810ef076
APARNA SHARMA <jmdaparna@gmail.com>
2026-07-18 15:25:46 +0530

    Time: 6 ms (74.49%), Space: 45.1 MB (68.33%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>>ans=new ArrayList<>();
+        List<Integer>curr=new ArrayList<>();
+        Arrays.sort(candidates);
+
+        backtrack(candidates,target,0,curr,ans);
+        return ans;
+    }
+    public void backtrack(int[] candidates,int target,int index,List<Integer>curr,List<List<Integer>>ans)
+    {
+        if(target==0){
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+        if(target<0) return;
+
+        for(int i=index;i<candidates.length;i++)
+        {
+            if(i>index && candidates[i]==candidates[i-1]) continue;
+            curr.add(candidates[i]);
+            backtrack(candidates,target-candidates[i],i+1,curr,ans);
+            curr.remove(curr.size()-1);
+        }
+    }
+}
