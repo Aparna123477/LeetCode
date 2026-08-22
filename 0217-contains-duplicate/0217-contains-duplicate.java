01261a483fb7b1f9359099623ba70fa333fc407e
APARNA SHARMA <jmdaparna@gmail.com>
2026-08-22 10:12:59 +0530

    Time: 25 ms (18.14%), Space: 81.7 MB (81.26%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,10 +1,10 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashMap<Integer,Boolean>map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-        if(map.containsKey(nums[i])) return true;
-        else map.put(nums[i],true);
-        
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i-1]==nums[i]) return true;
+        }
+        return false;
     }
-    return false;}
 }
