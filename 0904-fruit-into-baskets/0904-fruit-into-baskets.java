2b519dedf22565124f3538c5d0271d610e8f07a9
APARNA SHARMA <jmdaparna@gmail.com>
2026-08-22 23:04:24 +0530

    Time: 55 ms (43.3%), Space: 70.5 MB (56.56%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,8 +1,9 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        HashMap<Integer,Integer> map=new HashMap<>();
         int left=0,max=0;
-        for(int r=0;r<fruits.length;r++){
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int r=0;r<fruits.length;r++)
+        {
             map.put(fruits[r],map.getOrDefault(fruits[r],0)+1);
 
             while(map.size()>2){
@@ -10,11 +11,12 @@ class Solution {
                 if(map.get(fruits[left])==0){
                     map.remove(fruits[left]);
                 }
-                 left++;
+                left++;
             }
             max=Math.max(max,r-left+1);
         }
         return max;
+
         
     }
 }
