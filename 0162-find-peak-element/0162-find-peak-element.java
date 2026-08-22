39cad8fe84d21ac75a8afc6a4a90efd9366c66aa
APARNA SHARMA <jmdaparna@gmail.com>
2026-08-22 23:22:51 +0530

    Time: 0 ms (100%), Space: 44.3 MB (38.39%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,13 +1,14 @@
 class Solution {
     public int findPeakElement(int[] nums) {
         int max=Integer.MIN_VALUE;
-        int min=0;
+        int ans=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]>max){
                 max=nums[i];
-                min=i;
+                ans=i;
             }
         }
-        return min;
+        return ans;
+        
     }
 }
