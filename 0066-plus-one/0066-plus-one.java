e1a59e3fcef06520a60d1e907a89a8611d5aef06
APARNA SHARMA <jmdaparna@gmail.com>
2026-09-17 12:13:22 +0530

    Time: 0 ms (100%), Space: 43.7 MB (8.68%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,16 +1,17 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        //T(n)=O(n) ,S(n)=O(n)
-        for(int i=digits.length-1;i>=0;i=i-1){
-            if(digits[i]==9) 
-            digits[i]=0;
-
+        int n=digits.length;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(digits[i]==9){
+                digits[i]=0;
+            }
             else{
-                digits[i]=digits[i]+1;
+                digits[i]+=1;
                 return digits;
             }
         }
-        int[] res=new int[digits.length+1];
+        int[] res=new int[n+1];
         res[0]=1;
         return res;
     }
