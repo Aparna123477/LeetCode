21b4527f4744d2925eab225259f67b8c487fb7b7
APARNA SHARMA <jmdaparna@gmail.com>
2026-09-17 11:20:01 +0530

    Time: 0 ms (100%), Space: 47.3 MB (77.93%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int right_sum=0;
+        for(int i:nums){
+            right_sum+=i;
+        }
+        int left_sum=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(left_sum==right_sum-nums[i])return i;
+            left_sum+=nums[i];
+            right_sum-=nums[i];
+        }
+        return -1;
+    }
+}
